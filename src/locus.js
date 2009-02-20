4bda9e29fd3eb2ed8b4434e90d2f1c7ed5364f68
Larry Myers <larry@larrymyers.com>
2009-02-19 20:31:16 -0500

    added MIT license, added loki.js to test dir

--- src/locus.js
+++ src/locus.js
@@ -1,17 +1,45 @@
-(function() {
-  // preserve any preexisting geolocation implementation
-  var oldGeo = null;
-  if (typeof navigator.geolocation != 'undefined') {
-    oldGeo = navigator.geolocation;
-  }
-  
+/**
+ * Copyright (c) 2009 Larry Myers
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * Locus - A Geolocation Javascript Library
+ */
+(function() {  
   var obj = {};
   
-  function geode(successCallback, errorCallback, options) {
-    oldGeo.getCurrentPosition(successCallback, errorCallback);
+  function checkGeode() {
+    return (typeof navigator.geolocation != 'undefined');
   };
   
-  function loki(successCallback, errorCallback, options) {    
+  function doGeode(successCallback, errorCallback, options) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  };
+  
+  function checkLoki() {
+    return (typeof LokiAPI == 'function');
+  };
+  
+  function doLoki(successCallback, errorCallback, options) {    
     var loki = LokiAPI();
     loki.onSuccess = successCallback;
     loki.onFailure = errorCallback;
@@ -19,11 +47,47 @@
     loki.requestLocation(true,loki.NO_STREET_ADDRESS_LOOKUP);
   };
   
-  function gears(successCallback, errorCallback, options) {
+  function checkGears() {
+    return (typeof window.google != 'undefined');
+  };
+  
+  function doGears(successCallback, errorCallback, options) {
     var geo = google.gears.factory.create('beta.geolocation');
     geo.getCurrentPosition(successCallback, errorCallback);
   };
   
+  /**
+   * Check if the service is available on the client browser.
+   * Returns boolean value as result.
+   */
+  obj.check = function(serviceName) {
+    var flag = false;
+    
+    if (!serviceName || (typeof serviceName != 'String')) {
+      return flag;
+    }
+    
+    switch(serviceName.toLowerCase()) {
+      case 'loki':
+        flag = checkLoki();
+        break;
+      case 'geode':
+        flag = checkGeode();
+        break;
+      case 'gears':
+        flag = checkGears();
+        break;
+      default:
+        flag = false;
+    }
+    
+    return flag;
+  };
+  
+  /**
+   * Gets the current lat/lng of the client browser, and passes
+   * the result to the provided callback function.
+   */
   obj.getCurrentPosition = function(callback, errorCallback, options) {    
     if (typeof options == 'undefined') {
       options = {};
@@ -38,21 +102,20 @@
     }
     
     for (var i = 0; i < serviceList.length; i++) {
-      if (serviceList[i] == 'loki' && typeof LokiAPI == 'function' && options.LOKI_KEY) {
-        loki(callback, errorCallback, options);
+      if (serviceList[i] == 'loki' && options.LOKI_KEY && checkLoki()) {
+        doLoki(callback, errorCallback, options);
         return;
-      } else if (serviceList[i] == 'geode' && oldGeo != null) {
-        geode(callback, errorCallback, options);
+      } else if (serviceList[i] == 'geode' && checkGeode()) {
+        doGeode(callback, errorCallback, options);
         return;
-      } else if (serviceList[i] == 'gears' && window.google) {
-        gears(callback, errorCallback, options);
+      } else if (serviceList[i] == 'gears' && checkGears()) {
+        doGears(callback, errorCallback, options);
         return;
       }
     }
     
     errorCallback('No location services were found.');
   };
-  
-  // navigator.geolocation = obj;
+
   window.locus = obj;
 })();
